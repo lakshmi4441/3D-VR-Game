93671ed2bbeae7c657bac17dc2a4eada2ca435a8
lakshmi4441 <lakshmi4441@gmail.com>
2020-05-03 16:46:47 -0500

    Project-4

---
+++ Project-4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody playerRb;
+    public GameObject focalPoint;
+    public float speed = 5.0f;
+    public bool hasPowerup = false;
+    private float powerUpStrenght = 15.0f;
+    public GameObject powerupIndicator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.forward * speed * forwardInput);
+        playerRb.AddForce(Vector3.forward * forwardInput * speed);
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            powerupIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+        
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+          powerupIndicator.gameObject.SetActive(false);
+             
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            enemyRigidbody.AddForce(awayFromPlayer * powerUpStrenght, ForceMode.Impulse);
+            Debug.Log("Player collided with" + collision.gameObject + "with powerup set to" + hasPowerup);
+            
+        }
+    }
+} 
+  
